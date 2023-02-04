8346832ad2601af060f3869ab5ccee837cf8908a
Bublikass <jonas.lsm2@gmail.com>
2023-02-05 00:32:39 +0200

    asd

--- GameJam1/Assets/Scripts/Shop/ShopItem.cs
+++ GameJam1/Assets/Scripts/Shop/ShopItem.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ShopItem : MonoBehaviour
 {
     public PlantSO plant;
     public Item item;
     public Image icon;
+    [SerializeField]
+    private TextMeshProUGUI priceText;
 
+    [SerializeField] private bool isSelling;
     [SerializeField] private RectTransform errorPos;
+
+    private void Update()
+    {
+        SetPrice();
+    }
+
     public void BuyItem()
     {
         bool bought = PlayerInventory.Instance.BuyItem(plant);
@@ -29,5 +39,14 @@ public class ShopItem : MonoBehaviour
         bool sold = PlayerInventory.Instance.SellItem(item, item.amount);
         if (!sold)
             PlayerInventory.Instance.Create2DText("You don't have enough supplies!", errorPos.position, transform, PlayerInventory.Instance.textData2DDefault);
+    }
+
+    public void SetPrice()
+    {
+        if (isSelling)
+            priceText.text = item.sellPrice.ToString();
+        else if (!isSelling)
+            priceText.text = item.price.ToString();
+
     }
 }
